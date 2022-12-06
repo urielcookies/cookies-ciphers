da49b0fd5ec5207cf931293f2eff78de16eb9a07
Ever Uriel Garcia <urielcookies@outlook.com>
2022-12-06 14:25:36 -0700

    active cipher layout

--- src/Components/ActiveCipher.tsx
+++ src/Components/ActiveCipher.tsx
@@ -1,8 +1,18 @@
+import { isEqual, replace } from "lodash";
 import { FC } from "react";
+import { Location, useLocation } from "react-router-dom";
+
+import ReverseCipher from "./ReverseCipher";
+
 
 const ActiveCipher: FC = () => {
+	const location: Location = useLocation();
+	const activeCipher: string = replace(location.pathname, '/ciphers/', '');
+
 	return (
-		<div>ActiveCipher</div>
+		<>
+			{isEqual(activeCipher, 'reverse') && <ReverseCipher />}
+		</>
 	)
 };
 
