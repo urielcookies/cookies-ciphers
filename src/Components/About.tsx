164b253c70260985cbaf6be4d16e9c6d60fb01b6
Ever Uriel Garcia <urielcookies@outlook.com>
2022-12-08 15:08:22 -0700

    Hi Evelyn

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -3,7 +3,14 @@ import { FC } from 'react'
 
 const About: FC = () => {
 	return (
-		<div>About</div>
+		<div style={{
+			height: '100%',
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'center',
+		}}>
+			Hi Evelyn ❤️
+		</div>
 	)
 };
 
